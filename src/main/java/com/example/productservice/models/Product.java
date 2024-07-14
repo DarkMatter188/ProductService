bbf8fa1fd4cb2bca9d966163991eca99805d642c
Apoorv <apoorvranjan343@gmail.com>
2024-07-14 17:56:30 +0530

    Connected mysql db in spring

--- src/main/java/com/example/productservice/models/Product.java
+++ src/main/java/com/example/productservice/models/Product.java
@@ -1,12 +1,20 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+
+@Entity
 public class Product extends BaseModel {
     String title;
     double price;
+
+    @ManyToOne
     Category category;
+    //M --------------- 1
+    //Product ------ Category
 }
